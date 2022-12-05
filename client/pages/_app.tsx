65899847b1eca0f76aeaed5b81fc5b065f494dc9
Julien <julien7henry@gmail.com>
2022-12-05 15:30:21 +0100

    build(queryClient): setup client provider

--- client/pages/_app.tsx
+++ client/pages/_app.tsx
@@ -3,18 +3,28 @@ import { ThemeProvider } from "@mui/material/styles";
 import CssBaseline from "@mui/material/CssBaseline";
 import Layout from "../src/components/Layout";
 import { theme } from "../src/style/theme";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
 type AppProps<P = any> = {
   pageProps: P;
 } & Omit<NextAppProps<P>, "pageProps">;
 
 export default function MyApp({ Component, pageProps }: AppProps) {
+  const queryClient = new QueryClient({
+    defaultOptions: {
+      queries: {
+        refetchOnWindowFocus: false,
+      },
+    },
+  });
   return (
-    <ThemeProvider theme={theme}>
-      <CssBaseline />
-      <Layout>
-        <Component {...pageProps} />
-      </Layout>
-    </ThemeProvider>
+    <QueryClientProvider client={queryClient}>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <Layout>
+          <Component {...pageProps} />
+        </Layout>
+      </ThemeProvider>
+    </QueryClientProvider>
   );
 }
