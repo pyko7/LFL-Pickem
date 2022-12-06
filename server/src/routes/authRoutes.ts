3146ddc3d22df3463dfb0202eb229736c3af5a0d
Julien <julien7henry@gmail.com>
2022-12-06 11:11:37 +0100

    build(route): add confirm email route

--- server/src/routes/authRoutes.ts
+++ server/src/routes/authRoutes.ts
@@ -1,7 +1,9 @@
 import { Router } from "express";
 import { createSessionCookie } from "../controllers/auth/createSessionCookie";
 import { createUser } from "../controllers/auth/createUser";
+import { resendVerificationEmail } from "../controllers/auth/resendVerificationEmail";
 
 export const authRoutes = Router();
 authRoutes.post("/signup", createUser);
 authRoutes.post("/sessionLogin", createSessionCookie);
+authRoutes.post("/confirm-email", resendVerificationEmail);
