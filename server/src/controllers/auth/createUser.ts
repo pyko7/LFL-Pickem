536ad0189b82af2bcf8b4687b7190a20b998daf4
Julien <julien7henry@gmail.com>
2022-12-06 13:26:39 +0100

    feat(signup): add of cookie with email in token

--- server/src/controllers/auth/createUser.ts
+++ server/src/controllers/auth/createUser.ts
@@ -2,9 +2,13 @@ import { Request, Response } from "express";
 import { auth } from "../../firebase";
 import { createUserSchema } from "../../validations/userValidation";
 import { sendVerificationEmail } from "./sendVerificationEmail";
+import { sign } from "jsonwebtoken";
+import dotenv from "dotenv";
+dotenv.config();
 
 export const createUser = async (req: Request, res: Response) => {
   const user = req.body;
+  const pseudo = user.pseudo.toLowerCase();
   try {
     await createUserSchema.validate(user);
 
@@ -19,15 +23,23 @@ export const createUser = async (req: Request, res: Response) => {
       email: user.email,
       emailVerified: false,
       password: user.password,
-      displayName: user.pseudo,
+      displayName: pseudo,
       disabled: false,
     });
 
     const verificationEmail = await auth.generateEmailVerificationLink(
       user.email
     );
+    const token = sign(
+      {
+        exp: Math.floor(Date.now() / 1000) + 60 * 60,
+        data: user.email,
+      },
+      `${process.env.JWT_SECRET_KEY}`
+    );
     sendVerificationEmail(user.email, verificationEmail);
 
+    res.cookie("auth", token, { httpOnly: true, maxAge: 3600000 });
     res.status(201).json({ message: "User created!" });
   } catch (error) {
     if (error instanceof Error) {
