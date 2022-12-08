fd7630d3f43014f9c3acd7621cf807bcc9e34ccf
Julien <julien7henry@gmail.com>
2022-12-08 17:12:54 +0100

    feat(middleware): update csrf middleware

---
+++ server/src/middleware/createCsrfToken.ts
@@ -0,0 +1,17 @@
+import { doubleCsrf } from "csrf-csrf";
+import { Request, Response } from "express";
+
+export const { generateToken, doubleCsrfProtection } = doubleCsrf({
+  getSecret: () => `${process.env.SECRET_SESSION}`,
+  cookieName: "__Host.x-csrf-token",
+  cookieOptions: {
+    httpOnly: false,
+    sameSite: "lax",
+  },
+  getTokenFromRequest: (req) => req.headers["x-csrf-token"],
+});
+
+export const createCsrf = async (req: Request, res: Response) => {
+  const csrfToken = generateToken(res, req);
+  res.json({ csrfToken });
+};
