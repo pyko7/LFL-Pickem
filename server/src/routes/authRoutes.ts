3ec218ce7dffc462f03f932b546c51d92add3b37
Julien <julien7henry@gmail.com>
2022-12-08 17:17:13 +0100

    feat(cookie): remove session cookie on auth urls

--- server/src/routes/authRoutes.ts
+++ server/src/routes/authRoutes.ts
@@ -3,16 +3,18 @@ import { createSessionCookie } from "../controllers/auth/createSessionCookie";
 import { createUser } from "../controllers/auth/createUser";
 import { resendVerificationEmail } from "../controllers/auth/resendVerificationEmail";
 import { sendResetPasswordEmail } from "../controllers/auth/sendResetPasswordEmail";
-import { doubleCsrfProtection, loginToken } from "../middleware/csrfLogin";
+import { createCsrf } from "../middleware/createCsrfToken";
 
 export const authRoutes = Router();
 
+authRoutes.all("*", (req, res, next) => {
+  if (req.cookies.session) {
+    res.clearCookie("session");
+  }
+  next();
+});
+authRoutes.get("/login", createCsrf);
 authRoutes.post("/signup", createUser);
-authRoutes.post(
-  "/sessionLogin",
-  loginToken,
-  doubleCsrfProtection,
-  createSessionCookie
-);
+authRoutes.post("/sessionLogin", createSessionCookie);
 authRoutes.post("/confirm-email", resendVerificationEmail);
 authRoutes.post("/reset-password", sendResetPasswordEmail);
