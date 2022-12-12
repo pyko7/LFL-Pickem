5b0ad9a0a75e396f3707d949f443002b1b5aba7e
Julien <julien7henry@gmail.com>
2022-12-12 16:48:53 +0100

    fix(email): update email generation function

--- server/src/controllers/auth/resendVerificationEmail.ts
+++ server/src/controllers/auth/resendVerificationEmail.ts
@@ -1,14 +1,13 @@
 import { Request, Response } from "express";
 import { auth } from "../../firebase";
 import { sendVerificationEmail } from "./sendVerificationEmail";
-import { verify } from "jsonwebtoken";
 import dotenv from "dotenv";
 dotenv.config();
 
 export const resendVerificationEmail = async (req: Request, res: Response) => {
   const { email } = req.body;
   try {
-    const verificationEmail = await auth.generatePasswordResetLink(email);
+    const verificationEmail = await auth.generateEmailVerificationLink(email);
     sendVerificationEmail(email, verificationEmail);
     res.status(200).json({ message: "Email sent!" });
   } catch (error) {
