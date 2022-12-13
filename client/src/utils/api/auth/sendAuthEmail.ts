21efc0e4ea60d9ab5901290c2d1b9e71dde9aa02
Julien <julien7henry@gmail.com>
2022-12-13 09:38:13 +0100

    feat(email): update security and error handling

--- client/src/utils/api/auth/sendAuthEmail.ts
+++ client/src/utils/api/auth/sendAuthEmail.ts
@@ -1,20 +1,39 @@
+import { FirebaseError } from "firebase/app";
 import { AuthForm } from "~/src/types/forms";
+import Cookies from "js-cookie";
 
 export const sendAuthEmail = async (user: AuthForm, url: string) => {
+  const csrfToken = Cookies.get("__Host.x-csrf-token");
   const { email } = user;
-
-  const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/auth${url}`, {
-    method: "POST",
-    credentials: "include",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify({ email }),
-  });
-  const data = await res.json();
-  console.log(data);
-  if (!res.ok) {
-    throw new Error(data);
+  try {
+    const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/${url}`, {
+      method: "POST",
+      credentials: "include",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ email, csrfToken }),
+    });
+    const data = await res.json();
+    if (!res.ok) {
+      throw new Error(data);
+    }
+    return data;
+  } catch (error) {
+    if (error instanceof FirebaseError) {
+      if (error.code === "auth/internal-error") {
+        throw new Error(
+          "Une erreur s'est produite, veuillez réessayer ultérieurement"
+        );
+      }
+      if (error.code === "auth/too-many-requests") {
+        throw new Error(
+          "Votre compte a été temporairement bloqué. Pour pouvoir vous connecter, veuillez modifier votre mot de passe ou réessayer ultérieurement"
+        );
+      }
+    }
+    if (error instanceof Error) {
+      throw new Error(error.message);
+    }
   }
-  return data;
 };
