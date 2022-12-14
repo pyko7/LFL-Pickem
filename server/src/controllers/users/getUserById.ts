5807c33ae64b4c510089379417dd050d624cd151
Julien <julien7henry@gmail.com>
2022-12-14 14:39:15 +0100

    feat(profile): update response data type

--- server/src/controllers/users/getUserById.ts
+++ server/src/controllers/users/getUserById.ts
@@ -5,8 +5,9 @@ export const getUserById = async (req: Request, res: Response) => {
   const sessionId = req.cookies.session;
   try {
     const decodedToken = await auth.verifySessionCookie(sessionId, true);
-    const user = await auth.getUser(decodedToken.uid);
-    res.status(200).json(user.providerData);
+    const data = await auth.getUser(decodedToken.uid);
+    const user = data.providerData[0];
+    res.status(200).json(user);
   } catch (error) {
     res.status(400).json(error);
   }
