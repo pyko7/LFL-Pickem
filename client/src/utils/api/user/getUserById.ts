558509e0425c20aca7b670111ce406eb22e709f6
Julien <julien7henry@gmail.com>
2022-12-14 11:06:03 +0100

    feat(user): add get user information functions

---
+++ client/src/utils/api/user/getUserById.ts
@@ -0,0 +1,32 @@
+export const getUserById = async () => {
+  try {
+    const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/user`, {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      credentials: "include",
+    });
+    const data = await res.json();
+    if (!res.ok) {
+      if (res.status === 400) {
+        throw new Error("Bad request");
+      }
+      if (res.status === 401) {
+        if (data.code === "auth/argument-error") {
+          throw new Error("Token invalide");
+        }
+        throw new Error("Requête non autorisée");
+      }
+      throw new Error("Une erreur est survenue");
+    }
+    return data;
+  } catch (error) {
+    if (error instanceof Error) {
+      if (error.message === "Token invalide") {
+        window.location.href = "/login";
+      }
+      throw new Error("Une erreur est survenue");
+    }
+  }
+};
--- server/src/controllers/users/getUserById.ts
+++ server/src/controllers/users/getUserById.ts
@@ -6,7 +6,7 @@ export const getUserById = async (req: Request, res: Response) => {
   try {
     const decodedToken = await auth.verifySessionCookie(sessionId, true);
     const user = await auth.getUser(decodedToken.uid);
-    res.status(200).json(user);
+    res.status(200).json(user.providerData);
   } catch (error) {
     res.status(400).json(error);
   }
