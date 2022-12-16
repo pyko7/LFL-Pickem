554e5ea371816aebbdb0285fca55dc829a4e2261
Julien <julien7henry@gmail.com>
2022-12-16 16:51:00 +0100

    build(path): update file path

--- server/src/firebase.ts
+++ server/src/firebase.ts
@@ -2,7 +2,7 @@ import { initializeApp, cert } from "firebase-admin/app";
 import { getAuth } from "firebase-admin/auth";
 import { getFirestore } from "firebase-admin/firestore";
 
-const serviceAccount = require("../creds.json");
+const serviceAccount = require("../src/assets/json/creds.json");
 
 initializeApp({
   credential: cert(serviceAccount),
