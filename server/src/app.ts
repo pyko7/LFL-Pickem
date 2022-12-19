a6f0b9316b473c0c19ca9800cc5a0f413f6db647
Julien <julien7henry@gmail.com>
2022-12-19 14:51:27 +0100

    feat(images): add static images handling

--- server/src/app.ts
+++ server/src/app.ts
@@ -3,6 +3,7 @@ import helmet from "helmet";
 import cors from "cors";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
+import path from "path";
 import { authRoutes } from "./routes/authRoutes";
 import { userRoutes } from "./routes/userRoutes";
 import { verifySession } from "./middleware/verifySession";
@@ -15,9 +16,16 @@ const port = process.env.PORT;
 const app = express();
 app.use(express.json());
 app.use(cors({ credentials: true, origin: `${process.env.CLIENT_URL}` }));
-app.use(helmet());
+app.use(
+  helmet({
+    crossOriginResourcePolicy: {
+      policy: "same-site",
+    },
+  })
+);
 app.use(cookieParser());
 
+app.use(express.static(path.join(__dirname, "assets/images/logos")));
 app.use("/api/auth", authRoutes);
 app.use("/api/user", verifySession, userRoutes);
 app.use("/api/team", verifySession, teamRoutes);
