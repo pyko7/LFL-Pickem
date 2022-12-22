86fdb5e0ce66949cc4db91c0775d2680f4962e31
Julien <julien7henry@gmail.com>
2022-12-22 10:37:36 +0100

    feat(teams): add games by day with teams card

---
+++ client/src/components/Cards/SecondTeam.tsx
@@ -0,0 +1,83 @@
+import { styled, useTheme } from "@mui/material/styles";
+import Card from "@mui/material/Card";
+import CardContent from "@mui/material/CardContent";
+import Typography from "@mui/material/Typography";
+import CardActionArea from "@mui/material/CardActionArea";
+import useMediaQuery from "@mui/material/useMediaQuery";
+import { Team } from "~/src/types/teams";
+import Image from "next/image";
+
+type TeamProps = {
+  team: Team;
+};
+
+const SecondTeam = ({ team }: TeamProps) => {
+  const theme = useTheme();
+  const isBiggerThanMobile = useMediaQuery(theme.breakpoints.up("sm"));
+
+  const Team = styled(Card)(({ theme }) => ({
+    // width: selectedCard === 2 ? "75%" : "50%",
+    width: "50%",
+    minWidth: "33%",
+    heigth: 70,
+    color: theme.palette.neutral.light,
+    backgroundColor: "#000",
+    background: `linear-gradient(-90deg, ${theme.palette.primary.dark} 0%, ${team.color} 75%)`,
+    WebkitTransition: "width 1s ease-in-out",
+    MozTransition: "width 1s ease-in-out",
+    transition: "width 1s ease-in-out",
+    [theme.breakpoints.up("sm")]: {
+      minWidth: "25%",
+    },
+  }));
+
+  const ActionArea = styled(CardActionArea)({
+    width: "100%",
+    height: "100%",
+    display: "flex",
+    justifyContent: "flex-start",
+  });
+
+  const TeamContent = styled(CardContent)(({ theme }) => ({
+    width: "100%",
+    padding: 10,
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "flex-start",
+    gap: 10,
+    fontWeight: 700,
+    [theme.breakpoints.up("sm")]: {
+      width: "65%",
+      justifyContent: "space-between",
+      gap: 0,
+    },
+  }));
+
+  const TeamName = styled(Typography)(({ theme }) => ({
+    fontWeight: 700,
+    fontSize: 16,
+    [theme.breakpoints.up("sm")]: {
+      fontSize: 18,
+    },
+  }));
+
+  return (
+    <Team>
+      <ActionArea>
+        <TeamContent>
+          <Image
+            loader={() => team.logo}
+            unoptimized
+            src={team.logo}
+            alt={team.name}
+            width={isBiggerThanMobile ? 60 : 40}
+            height={isBiggerThanMobile ? 60 : 40}
+          />
+          <TeamName variant="h2">{team.name}</TeamName>
+        </TeamContent>
+      </ActionArea>
+    </Team>
+  );
+};
+
+export default SecondTeam;
---
+++ client/src/components/Containers/GameContainer.tsx
@@ -0,0 +1,40 @@
+import { useEffect, useState } from "react";
+import { styled } from "@mui/material/styles";
+import Box from "@mui/material/Box";
+import FirstTeam from "../Cards/FirstTeam";
+import SecondTeam from "../Cards/SecondTeam";
+import { Game, Team } from "~/src/types/teams";
+import { useGameContext } from "~/context/GameContext";
+
+const GameContainer = (props: Game) => {
+  const [firstTeam, setFirstTeam] = useState<Team | undefined>();
+  const [secondTeam, setSecondTeam] = useState<Team | undefined>();
+  const { teams } = useGameContext();
+
+  useEffect(() => {
+    teams?.teams.forEach((team) => {
+      if (team.id === props.firstTeamId) {
+        setFirstTeam(team);
+      }
+      if (team.id === props.secondTeamId) {
+        setSecondTeam(team);
+      }
+    });
+  });
+
+  const Game = styled(Box)({
+    width: "100%",
+    display: "flex",
+    justifyContent: "space-between",
+    gap: 10,
+  });
+
+  return (
+    <Game>
+      {firstTeam ? <FirstTeam team={firstTeam} /> : null}
+      {secondTeam ? <SecondTeam team={secondTeam} /> : null}
+    </Game>
+  );
+};
+
+export default GameContainer;
