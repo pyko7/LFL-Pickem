a0c35633a4ced159b31d01e78d48132b45b607b2
Julien Henry <julienhenry967@gmail.com>
2023-01-04 18:18:40 +0100

    refactor(typo): update typo

--- client/src/components/Navigation/ScrollableDaysTabs.tsx
+++ client/src/components/Navigation/ScrollableDaysTabs.tsx
@@ -1,4 +1,4 @@
-import { use, useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import { styled } from "@mui/material/styles";
 import Box from "@mui/material/Box";
 import CircularProgress from "@mui/material/CircularProgress";
