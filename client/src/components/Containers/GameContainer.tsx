c99a9ccaef74d747c5511cca2c99c588899a6062
Julien Henry <julienhenry967@gmail.com>
2023-01-10 15:12:39 +0100

    feat(bet): add icon if it's a winner/loser bet

--- client/src/components/Containers/GameContainer.tsx
+++ client/src/components/Containers/GameContainer.tsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react";
-import { styled } from "@mui/material/styles";
+import { styled, useTheme } from "@mui/material/styles";
+import useMediaQuery from "@mui/material/useMediaQuery";
 import Box from "@mui/material/Box";
 import CircularProgress from "@mui/material/CircularProgress";
 import FirstTeam from "../Cards/FirstTeam";
@@ -16,8 +17,11 @@ import { utcToZonedTime } from "date-fns-tz";
 import { isBefore, parseISO } from "date-fns";
 import { useMutation } from "@tanstack/react-query";
 import ErrorModal from "../Feedbacks/ErrorModal";
+import CheckCircleIcon from "@mui/icons-material/CheckCircle";
+import CancelIcon from "@mui/icons-material/Cancel";
 
 const GameContainer = (props: Game) => {
+  const theme = useTheme();
   const [firstTeam, setFirstTeam] = useState<Team | undefined>();
   const [secondTeam, setSecondTeam] = useState<Team | undefined>();
   const [disabledDay, setDisabledDay] = useState(false);
@@ -28,6 +32,8 @@ const GameContainer = (props: Game) => {
   const [errorMessage, setErrorMessage] = useState("");
 
   const { teams, userSelection } = useGameContext();
+
+  const isBiggerThanMobile = useMediaQuery(theme.breakpoints.up("sm"));
 
   const createBet = useMutation({
     mutationFn: addSelectedTeams,
@@ -158,26 +164,64 @@ const GameContainer = (props: Game) => {
     gap: 10,
   });
 
+  const SuccessIcon = styled(CheckCircleIcon)(({ theme }) => ({
+    position: "absolute",
+    top: "50%",
+    transform: "translateY(-50%)",
+    width: 25,
+    height: 25,
+    zIndex: 1,
+    color: theme.palette.secondary.main,
+  }));
+
+  const FailIcon = styled(CancelIcon)(({ theme }) => ({
+    position: "absolute",
+    top: "50%",
+
+    transform: "translateY(-50%)",
+    width: 25,
+    height: 25,
+    zIndex: 1,
+    color: theme.palette.error.main,
+  }));
+
   return (
     <Game>
       {firstTeam && secondTeam ? (
-        <Box
-          sx={{
-            width:
-              selectedTeam === firstTeam.id
-                ? "75%"
-                : selectedTeam === secondTeam.id
-                ? "33%"
-                : "50%",
-          }}
-          onClick={() => handleClick(firstTeam.id, secondTeam.id)}
-        >
-          <FirstTeam
-            team={firstTeam}
-            notSelected={notSelected}
-            disabledDay={disabledDay}
-          />
-        </Box>
+        <>
+          <Box
+            sx={{
+              position: "relative",
+              width:
+                selectedTeam === firstTeam.id
+                  ? "75%"
+                  : selectedTeam === secondTeam.id
+                  ? "33%"
+                  : "50%",
+            }}
+            onClick={() => handleClick(firstTeam.id, secondTeam.id)}
+          >
+            {!disabledDay ? null : selectedTeam !==
+              firstTeam.id ? null : firstTeam.id === props.winner ? (
+              <SuccessIcon
+                sx={{
+                  left: isBiggerThanMobile ? 15 : 5,
+                }}
+              />
+            ) : (
+              <FailIcon
+                sx={{
+                  left: isBiggerThanMobile ? 15 : 5,
+                }}
+              />
+            )}
+            <FirstTeam
+              team={firstTeam}
+              notSelected={notSelected}
+              disabledDay={disabledDay}
+            />
+          </Box>
+        </>
       ) : null}
       {createBet.isLoading || updateBet.isLoading || deleteBet.isLoading ? (
         <CircularProgress color="secondary" />
@@ -185,6 +229,7 @@ const GameContainer = (props: Game) => {
       {firstTeam && secondTeam ? (
         <Box
           sx={{
+            position: "relative",
             width:
               selectedTeam === secondTeam.id
                 ? "75%"
@@ -199,6 +244,20 @@ const GameContainer = (props: Game) => {
             notSelected={notSelected}
             disabledDay={disabledDay}
           />
+          {!disabledDay ? null : selectedTeam !==
+            secondTeam.id ? null : secondTeam.id === props.winner ? (
+            <SuccessIcon
+              sx={{
+                right: isBiggerThanMobile ? 15 : 5,
+              }}
+            />
+          ) : (
+            <FailIcon
+              sx={{
+                right: isBiggerThanMobile ? 15 : 5,
+              }}
+            />
+          )}
         </Box>
       ) : null}
       {betError ? (
