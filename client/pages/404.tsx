ab78a3b8059a13990f64b047247f12f3f3b3e183
Julien Henry <julienhenry967@gmail.com>
2023-01-12 16:40:20 +0100

    feat(404): add custom 404 page

---
+++ client/pages/404.tsx
@@ -0,0 +1,42 @@
+import Link from "next/link";
+import Container from "@mui/material/Container";
+import Typography from "@mui/material/Typography";
+
+const Custom404 = () => {
+  return (
+    <Container
+      sx={{
+        width: 1,
+        height: "100vh",
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+        textAlign: "center",
+        gap: 6,
+        color: "#fff",
+      }}
+    >
+      <Typography variant="h1" sx={{ fontSize: 52 }}>
+        Oops
+      </Typography>
+      <Typography sx={{ fontSize: 28 }}>Cette page n'existe pas </Typography>
+
+      <Link
+        href="/"
+        style={{
+          width: "100%",
+          maxWidth: 275,
+          padding: "10px 15px",
+          backgroundColor: "#B8933A",
+          color: "#000",
+          textDecoration: "none",
+          borderRadius: 10,
+        }}
+      >
+        Accueil
+      </Link>
+    </Container>
+  );
+};
+
+export default Custom404;
