9706642f61081afe48ab1bea780b05bb9a9630dc
Julien Henry <julienhenry967@gmail.com>
2023-01-19 16:06:46 +0100

    style(image): reduce image size on desktop

--- client/src/components/Cards/FirstTeam.tsx
+++ client/src/components/Cards/FirstTeam.tsx
@@ -69,9 +69,8 @@ const FirstTeam = ({ team, notSelected, disabledDay }: TeamProps) => {
             unoptimized
             src={team.logo}
             alt={team.name}
-            width={isBiggerThanMobile ? 60 : 40}
-            height={isBiggerThanMobile ? 60 : 40}
-            sizes="100vw"
+            width={isBiggerThanMobile ? 50 : 40}
+            height={isBiggerThanMobile ? 50 : 40}
             style={{
               objectFit: "contain",
             }}
