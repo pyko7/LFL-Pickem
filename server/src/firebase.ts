b4ebd5b54e45b542eea88b3fc90f49f853685172
Julien Henry <julienhenry967@gmail.com>
2023-01-24 10:29:56 +0100

    build(credentials): update sdk import

--- server/src/firebase.ts
+++ server/src/firebase.ts
@@ -1,12 +1,18 @@
 import { initializeApp, cert } from "firebase-admin/app";
 import { getAuth } from "firebase-admin/auth";
-import { getFirestore } from "firebase-admin/firestore";
+import dotenv from "dotenv";
+dotenv.config();
 
-const serviceAccount = require("../src/assets/json/creds.json");
+const serviceAccount = {
+  projectId: `${process.env.FIREBASE_PROJECT_ID}`,
+  clientEmail: `${process.env.FIREBASE_CLIENT_EMAIL}`,
+  privateKey: process.env.FIREBASE_PRIVATE_KEY
+    ? process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/gm, "\n")
+    : undefined,
+};
 
 initializeApp({
   credential: cert(serviceAccount),
 });
 
 export const auth = getAuth();
-export const db = getFirestore();
