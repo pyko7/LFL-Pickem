8d3f9effbee362fc3bd0b88384ef17cbe6abbe3f
Julien Henry <julienhenry967@gmail.com>
2023-01-26 15:26:06 +0100

    feat(csrf): update cookie type

--- server/src/middleware/createCsrfToken.ts
+++ server/src/middleware/createCsrfToken.ts
@@ -3,15 +3,18 @@ import { Request, Response } from "express";
 
 const { generateToken } = doubleCsrf({
   getSecret: () => `${process.env.SECRET_SESSION}`,
-  cookieName: "__Host.x-csrf-token",
+  cookieName: "__Host-.x-csrf-token",
   cookieOptions: {
-    httpOnly: false,
+    // httpOnly: true,
+    secure: true,
     sameSite: "lax",
+    path: "/",
   },
   getTokenFromRequest: (req) => req.headers["x-csrf-token"],
 });
 
 export const createCsrf = async (req: Request, res: Response) => {
   const csrfToken = generateToken(res, req);
+  console.log(csrfToken);
   res.json({ csrfToken });
 };
