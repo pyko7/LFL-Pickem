07bd1f6a58c254780a013846548007ecec4727fd
Julien Henry <julienhenry967@gmail.com>
2023-01-27 14:17:31 +0100

    feat(csrf): remove unused middlewares & endpoints

--- server/src/routes/authRoutes.ts
+++ server/src/routes/authRoutes.ts
@@ -4,12 +4,10 @@ import { createUser } from "../controllers/auth/createUser";
 import { logoutUser } from "../controllers/auth/logoutUser";
 import { resendVerificationEmail } from "../controllers/auth/resendVerificationEmail";
 import { sendResetPasswordEmail } from "../controllers/auth/sendResetPasswordEmail";
-import { createCsrf } from "../middleware/createCsrfToken";
 import { verifyUserEmail } from "../middleware/verifyUserEmail";
 
 export const authRoutes = Router();
 
-authRoutes.get("/csrf", createCsrf);
 authRoutes.post("/signup", createUser);
 authRoutes.post("/sessionLogin", verifyUserEmail, createSessionCookie);
 authRoutes.post("/confirm-email", resendVerificationEmail);
--- server/src/routes/userRoutes.ts
+++ server/src/routes/userRoutes.ts
@@ -4,12 +4,10 @@ import { deleteUser } from "../controllers/users/deleteUser";
 import { getUserById } from "../controllers/users/getUserById";
 import { getUserRank } from "../controllers/users/getUserRank";
 import { getUserScore } from "../controllers/users/getUserScore";
-import { createCsrf } from "../middleware/createCsrfToken";
 import { verifyUser } from "../middleware/verifyUser";
 
 export const userRoutes = Router();
 
-userRoutes.get("/token", createCsrf);
 userRoutes.get("/", getUserById);
 userRoutes.get("/score", getUserScore);
 userRoutes.get("/rank", getUserRank);
