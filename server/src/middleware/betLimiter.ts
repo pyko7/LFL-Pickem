ae185ee176b6ea0ec933aa5a05cd51d468525f2a
Julien Henry <julienhenry967@gmail.com>
2023-01-31 12:08:05 +0100

    feat(bet): reduce request rate

--- server/src/middleware/betLimiter.ts
+++ server/src/middleware/betLimiter.ts
@@ -3,7 +3,7 @@ import rateLimit from "express-rate-limit";
 
 export const betLimiter = rateLimit({
   windowMs: 1 * 60 * 1000, // 1 minutes
-  max: 50, // Limit each IP to 100 requests per `window` (here, per minute)
+  max: 35, // Limit each IP to 100 requests per `window` (here, per minute)
   standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
   legacyHeaders: false, // Disable the `X-RateLimit-*` headers
   message: (req: Request, res: Response) => {
