2cc2894c3d23e45a0111c5eeea2460363ac87ce7
Julien Henry <julienhenry967@gmail.com>
2023-02-01 10:26:52 +0100

    feat(logout): add domain to clear cookies on prod

--- server/src/controllers/auth/logoutUser.ts
+++ server/src/controllers/auth/logoutUser.ts
@@ -1,7 +1,17 @@
 import { Request, Response } from "express";
 
 export const logoutUser = (req: Request, res: Response) => {
-  res.clearCookie("session");
-  res.clearCookie("pid");
+  res.clearCookie("session", {
+    domain:
+      process.env.NODE_ENV === "production"
+        ? process.env.PROD_DOMAIN
+        : process.env.ENV_DOMAIN,
+  });
+  res.clearCookie("pid", {
+    domain:
+      process.env.NODE_ENV === "production"
+        ? process.env.PROD_DOMAIN
+        : process.env.ENV_DOMAIN,
+  });
   res.status(200).json({ message: "success" });
 };
