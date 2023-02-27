e06cf97c3e74fa4d9fa70212115e9d27ebbc9369
julien henry <julienhenry967@gmail.com>
2023-02-27 10:58:38 +0100

    style: rework rules page

--- client/pages/rules/index.tsx
+++ client/pages/rules/index.tsx
@@ -1,75 +1,27 @@
-import { styled } from "@mui/material/styles";
-import Box from "@mui/material/Box";
-import List from "@mui/material/List";
-import ListItem from "@mui/material/ListItem";
-import Typography from "@mui/material/Typography";
-
 const Rules = () => {
-  const Page = styled(Box)(({ theme }) => ({
-    position: "absolute",
-    top: "15%",
-    left: "50%",
-    transform: "translateX(-50%)",
-    width: "100%",
-    maxWidth: 1920,
-    padding: "35px 20px",
-    color: theme.palette.neutral.light,
-    backgroundColor: theme.palette.primary.main,
-  }));
-
-  const Title = styled(Typography)({
-    fontSize: 32,
-    fontWeight: 700,
-    textAlign: "center",
-  });
-
-  const TextContainer = styled(List)(({ theme }) => ({
-    width: "50%",
-    paddingTop: 35,
-    margin: "0 auto",
-    display: "flex",
-    flexDirection: "column",
-    alignItems: "center",
-    textAlign: "center",
-    gap: 20,
-    [theme.breakpoints.down("sm")]: {
-      width: "100%",
-    },
-  }));
-
-  const Rule = styled(ListItem)(({ theme }) => ({
-    width: "100%",
-    heigth: 60,
-    padding: "15px 25px ",
-    borderRadius: 4,
-    color: theme.palette.neutral.light,
-    backgroundColor: theme.palette.primary.light,
-    "&:hover": {
-      filter: "brightness(1.1)",
-    },
-  }));
-
   return (
-    <Page>
-      <Title>Règles</Title>
-      <TextContainer>
-        <Rule>Prédisez les vainqueurs de chaque journée de LFL.</Rule>
-        <Rule>
+    <section className="absolute top-[15%] left-1/2 -translate-x-1/2 w-full max-w-[1920px] py-9 px-5 text-neutral-light bg-main">
+      <h1 className="text-3xl font-bold text-center">Règles</h1>
+      <ul className="w-full pt-9 my-0 mx-auto flex flex-col items-center gap-5 md:w-1/2">
+        <li className="w-full  py-4 px-6 rounded-md text-neutral-light bg-main-light hover:brightness-[1.1]">
+          Prédisez les vainqueurs de chaque journée de LFL.
+        </li>
+        <li className="w-full  py-4 px-6 rounded-md text-neutral-light bg-main-light hover:brightness-[1.1]">
           En cas de bonne prédiction, vous gagnez 5 points. Si vous avez fait le
           mauvais choix, vous ne perdez pas de points mais vous n&lsquo;en
           gagnez pas non plus.
-        </Rule>
-        <Rule>
+        </li>
+        <li className="w-full  py-4 px-6 rounded-md text-neutral-light bg-main-light hover:brightness-[1.1]">
           Vous avez jusqu&lsquo;à 18h pour prédire sur les matchs du jour même.
-        </Rule>
-        <Rule>
+        </li>
+        <li className="w-full  py-4 px-6 rounded-md text-neutral-light bg-main-light hover:brightness-[1.1]">
           Les journées se déroulant généralement le mercredi soir et le jeudi
           soir. Les résultats sont mis à jour le lendemain des journées à 7h.
-        </Rule>
-        {/* <Rule>
+        </li>
+        {/* <li className="w-full  py-4 px-6 rounded-md text-neutral-light bg-main-light hover:brightness-[1.1]">
           Réaliser un score parfait sur une journée vous octroie un bonus de 3 points
-        </Rule> */}
-        {/* <Rule>
+        </li> */}
+        {/* <li className="w-full  py-4 px-6 rounded-md text-neutral-light bg-main-light hover:brightness-[1.1]">
           Vous pouvez rejoindre le&nbsp;
           <a
             href="https://discord.gg/g3M24pdShN"
@@ -80,8 +32,8 @@ const Rules = () => {
             discord
           </a>
           &nbsp; afin de partager vos résultats et prédictions.
-        </Rule>
-        <Rule>
+        </li>
+        <li className="w-full  py-4 px-6 rounded-md text-neutral-light bg-main-light hover:brightness-[1.1]">
           En cas de problème, un salon dedié existe sur le&nbsp;
           <a
             href="https://discord.gg/g3M24pdShN"
@@ -92,10 +44,12 @@ const Rules = () => {
             discord
           </a>
           .
-        </Rule> */}
-        <Rule>Amusez-vous :{")"}</Rule>
-      </TextContainer>
-    </Page>
+        </li> */}
+        <li className="w-full  py-4 px-6 rounded-md text-neutral-light bg-main-light hover:brightness-[1.1]">
+          Amusez-vous :{")"}
+        </li>
+      </ul>
+    </section>
   );
 };
 
