e720688a532bccaa4e1aafb0b655b24968bd9f63
julien henry <julienhenry967@gmail.com>
2023-02-28 11:32:20 +0100

    style: rework confirm email page

--- client/pages/signup/confirm-email.tsx
+++ client/pages/signup/confirm-email.tsx
@@ -1,127 +1,71 @@
 import { useState } from "react";
-import { styled, useTheme } from "@mui/material/styles";
-import Box from "@mui/material/Box";
-import Button from "@mui/material/Button";
-import Typography from "@mui/material/Typography";
-import Divider from "@mui/material/Divider";
-import LoginIcon from "@mui/icons-material/Login";
-import EmailIcon from "@mui/icons-material/Email";
 import { useRouter } from "next/router";
 import ConfirmEmailModal from "@/src/components/Modals/ConfirmEmailModal";
+import { ArrowRightOnRectangleIcon } from "@heroicons/react/24/outline";
+import { EnvelopeIcon } from "@heroicons/react/24/outline";
+import AuthModal from "@/src/components/Modals/AuthModal";
 
 const ConfirmEmail = () => {
-  const theme = useTheme();
   const { push } = useRouter();
   const [open, setOpen] = useState(false);
+  const [userAuth, setUserAuth] = useState(false);
 
   const formProps = { open, setOpen };
+  const authProps = { userAuth, setUserAuth };
 
   const handleLoginPageClick = () => {
-    return push("/login");
+    return userAuth ? setUserAuth(false) : setUserAuth(true);
   };
 
   const handleVerificationEmailClick = () => {
     return open ? setOpen(false) : setOpen(true);
   };
 
-  const Page = styled(Box)(({ theme }) => ({
-    position: "absolute",
-    top: 0,
-    left: "50%",
-    transform: "translateX(-50%)",
-    width: "100%",
-    maxWidth: 1920,
-    padding: "35px 20px",
-    color: theme.palette.neutral.light,
-    backgroundColor: theme.palette.primary.main,
-  }));
-
-  const Title = styled(Typography)({
-    fontSize: 32,
-    fontWeight: 700,
-    textAlign: "center",
-  });
-
-  const TextContainer = styled(Box)({
-    width: "100%",
-    marginTop: 35,
-    display: "flex",
-    flexDirection: "column",
-    gap: 20,
-    [theme.breakpoints.up("sm")]: {
-      width: "75%",
-      margin: "50px auto",
-    },
-  });
-
-  const SectionDivider = styled(Divider)(({ theme }) => ({
-    width: "66%",
-    maxWidth: 650,
-    margin: "auto",
-    backgroundColor: theme.palette.primary.light,
-  }));
-
-  const Buttons = styled(Button)(({ theme }) => ({
-    width: "fit-content",
-    color: theme.palette.neutral.light,
-    textTransform: "none",
-    fontSize: 16,
-    border: `1px solid ${theme.palette.primary.light}`,
-    "&:hover": {
-      border: `1px solid ${theme.palette.primary.light}`,
-
-      backgroundColor: theme.palette.primary.light,
-    },
-  }));
-
   return (
-    <Page component="section">
-      <Title variant="h1">Email de confirmation</Title>
-      <TextContainer>
-        <Typography variant="h2" sx={{ fontSize: 24, fontWeight: 400 }}>
-          Bienvenue !
-        </Typography>
-        <Typography paragraph>
-          Vous êtes bientôt prêt à accéder au site. Nous avons envoyé un email
+    <section className="px-4 text-neutral-light">
+      <h1 className="text-2xl font-bold text-center sm:text-3xl">Email de confirmation</h1>
+      <div className="w-full mt-8 flex flex-col gap-5 sm:w-3/4 sm:my-12 sm:mx-auto">
+        <h2 className="text-2xl font-medium">Bienvenue !</h2>
+        <p>
+          Vous êtes bientôt prêt à jouer. Nous avons envoyé un email
           de vérification lors de votre inscription.
           <br />
           <br /> Vérifiez votre boîte mail ainsi que vos{" "}
-          <strong style={{ color: theme.palette.secondary.main }}>
-            spams
-          </strong>{" "}
-          et cliquez sur le lien contenu dans l&apos;email.
+          <strong className="text-secondary">spams</strong> et cliquez sur le
+          lien contenu dans l&apos;email.
           <br />
           <br /> Note: l&apos;envoi de l&apos;email peut prendre quelques
           minutes.
-        </Typography>
-      </TextContainer>
-      <SectionDivider />
-      <TextContainer>
-        <Typography variant="h3" sx={{ fontSize: 18 }}>
-          Déjà confirmé ?
-        </Typography>
-        <Buttons
-          variant="outlined"
-          startIcon={<LoginIcon />}
+        </p>
+      </div>
+      <hr className="w-4/5 my-12 max-w-2xl mx-auto border-main-light" />
+
+      <div className="w-full mb-12 flex flex-col gap-5 sm:w-3/4">
+        <h3 className="text-lg">Déjà confirmé ?</h3>
+        <button
+          type="button"
+          className="w-fit pl-2 pr-5 pt-2.5 pb-2 flex items-center gap-2 rounded text-xs font-medium uppercase border-[1px] border-neutral-light shadow-md hover:shadow-lg"
           onClick={handleLoginPageClick}
         >
+          <ArrowRightOnRectangleIcon className="w-7 h-7" aria-hidden="true" />
           Page de connexion
-        </Buttons>
-      </TextContainer>
-      <TextContainer>
-        <Typography variant="h3" sx={{ fontSize: 18 }}>
-          Vous n&apos;avez pas reçu de mail ?
-        </Typography>
-        <Buttons
-          variant="outlined"
-          startIcon={<EmailIcon />}
+        </button>
+      </div>
+
+      <div className="w-full flex flex-col gap-5 sm:w-3/4">
+        <h3 className="text-lg">Vous n&apos;avez pas reçu de mail ?</h3>
+        <button
+          type="button"
+          className="w-fit pl-2 pr-5 pt-2.5 pb-2 flex items-center gap-2 rounded text-xs font-medium uppercase border-[1px] border-neutral-light shadow-md hover:shadow-lg"
           onClick={handleVerificationEmailClick}
         >
+          <EnvelopeIcon className="w-7 h-7" aria-hidden="true" />
           Renvoyer un email
-        </Buttons>
-      </TextContainer>
+        </button>
+      </div>
       {!open ? null : <ConfirmEmailModal {...formProps} />}
-    </Page>
+      {!userAuth ? null : <AuthModal {...authProps} />}
+    </section>
   );
 };
 
