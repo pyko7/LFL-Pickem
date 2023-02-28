627239b02ca140f0792babae157565175321a444
julien henry <julienhenry967@gmail.com>
2023-02-28 11:09:17 +0100

    style: update display for responsive

--- client/src/components/Modals/FormModal.tsx
+++ client/src/components/Modals/FormModal.tsx
@@ -7,7 +7,7 @@ interface Props {
   open: boolean;
   setOpen: (open: boolean) => void;
   title: string;
-  description: string;
+  description?: string;
   children?: ReactNode;
 }
 
@@ -20,7 +20,7 @@ const FormModal = ({ open, setOpen, title, description, children }: Props) => {
     <Transition show={open}>
       <Dialog open={open} onClose={handleClose} className="relative z-50">
         <Transition.Child
-          enter="transition-opacity ease-linear duration-300"
+          enter="transition-opacity ease-in-out duration-300"
           enterFrom="opacity-0"
           enterTo="opacity-100"
           leave="transition-opacity ease-linear duration-100"
@@ -29,21 +29,29 @@ const FormModal = ({ open, setOpen, title, description, children }: Props) => {
         >
           <div className="fixed inset-0 bg-black/90" aria-hidden="true" />
 
-          <div className="fixed inset-0 flex items-center justify-center p-4">
-            <Dialog.Panel className="w-fit py-6 px-4 flex flex-col items-center gap-6 rounded-md bg-main text-neutral-light md:max-w-lg md:px-6">
-              <button role="button" className="w-full flex flex-row-reverse text-neutral-light ">
-                <XMarkIcon className="w-7 h-7" onClick={handleClose}/>
-              </button>
-              <Dialog.Title className="text-center text-xl">
-                {title}
-              </Dialog.Title>
+          <div className="fixed inset-0 overflow-y-auto">
+            <div className="flex w-full min-h-full items-center justify-center p-4">
+              <Dialog.Panel className="w-full py-6 px-4 flex flex-col items-center gap-6 rounded-md bg-main text-neutral-light md:max-w-lg md:px-6">
+                <div className="w-full px-2 flex flex-row-reverse">
+                  <button
+                    type="button"
+                    className="w-7 h-7 flex flex-row-reverse text-neutral-light focus-visible:p-0"
+                  >
+                    <XMarkIcon className="w-full" onClick={handleClose} />
+                  </button>
+                </div>
+                <Dialog.Title className="text-center text-xl">
+                  {title}
+                </Dialog.Title>
+                {!description ? null : (
+                  <p className="max-w-md my-2 mx-auto px-[2px] whitespace-pre-line">
+                    {description}
+                  </p>
+                )}
 
-              <p className="max-w-md my-2 mx-auto px-[2px] whitespace-pre-line">
-                {description}
-              </p>
-
-              {children}
-            </Dialog.Panel>
+                {children}
+              </Dialog.Panel>
+            </div>
           </div>
         </Transition.Child>
       </Dialog>
