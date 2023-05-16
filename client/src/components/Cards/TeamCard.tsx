10542ea597417e2d9cc10683f1b8be365bd82b82
julien henry <julienhenry967@gmail.com>
2023-05-16 15:57:45 +0200

    fix: accessibility issues

--- client/src/components/Cards/TeamCard.tsx
+++ client/src/components/Cards/TeamCard.tsx
@@ -1,23 +1,22 @@
-import { useState } from "react";
+import { HTMLAttributes } from "react";
 import { Team } from "@/src/types/teams";
 import Image from "next/image";
 import { useThemeContext } from "@/context/ThemeContext";
 
-type Props = {
+type Props = HTMLAttributes<HTMLElement> & {
+  selected: boolean;
   team: Team;
   winningBet: null | boolean;
 };
 
-const TeamCard = ({ team, winningBet }: Props) => {
+const TeamCard = ({ selected, team, winningBet, ...rest }: Props) => {
   const { leagueId } = useThemeContext();
-  const [selected, setSelected] = useState(false);
 
   return (
-    <button
-      type="button"
+    <article
       aria-label="select winning team"
       className={`w-full py-2 px-4 flex items-center justify-between rounded-xl bg-neutral-800
-       border-1 border-transparent shadow-md outline-1 outline-blue-400
+       border-1 border-transparent shadow-md cursor-pointer outline-1 outline-blue-400
        ${selected ? "bg-neutral-900" : ""}
       ${
         selected && leagueId === 1
@@ -34,6 +33,7 @@ const TeamCard = ({ team, winningBet }: Props) => {
           : ""
       }
       hover:bg-neutral-900`}
+      {...rest}
     >
       <div className="w-full flex items-center gap-2">
         <div className="relative w-10 h-10">
@@ -48,7 +48,7 @@ const TeamCard = ({ team, winningBet }: Props) => {
         </div>
         <div className="font-semibold">{team.name}</div>
       </div>
-    </button>
+    </article>
   );
 };
 
