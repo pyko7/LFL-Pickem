4fa65e29d85efa8be611e5aa7f251dcafb69faac
julien henry <julienhenry967@gmail.com>
2023-05-22 08:38:58 +0200

    feat: update update score function

--- server/src/controllers/users/updateUserScore.ts
+++ server/src/controllers/users/updateUserScore.ts
@@ -1,57 +1,37 @@
 import { Request, Response } from "express";
 import { auth } from "../../firebase";
 import prisma from "../../prisma";
-import { PrismaBet, PrismaGame } from "../../types/prisma";
+import { calculateScore } from "../../utils/users/score/calculateScore";
 
 export const updateUserScore = async (req: Request, res: Response) => {
   const sessionId = req.cookies.session;
-  let userScore = 0;
-
   try {
     const decodedToken = await auth.verifySessionCookie(sessionId);
     const user = await prisma.user.findUnique({
       where: {
         id: decodedToken.uid,
       },
-      include: {
-        bets: true,
-      },
-    });
-    const allGames = await prisma.game.findMany();
-
-    if (!user || !allGames) {
-      throw new Error("Invalid request");
-    }
-
-    user.bets.map((bet: PrismaBet) => {
-      const betGames = allGames.filter(
-        (game: PrismaGame) => bet.gameId === game.id
-      );
-      const winnerBet = betGames.filter(
-        (game: PrismaGame) => bet.teamId === game.winner
-      );
-      for (let i = 0; i < winnerBet.length; i++) {
-        userScore += 3;
-      }
+      include: { bets: { include: { game: true } } },
     });
 
-    if (userScore - 25 === user.points) {
-      return (userScore += 5);
+    if (!user) {
+      throw new Error("User not found");
+    }
+    const { bets, points } = user;
+    const updatedPoints = calculateScore(bets);
+
+    if (updatedPoints > points) {
+      await prisma.user.update({
+        where: {
+          id: decodedToken.uid,
+        },
+        data: {
+          points: updatedPoints,
+        },
+      });
     }
 
-    if (userScore === user.points) {
-      return res.status(200).json(user?.points);
-    }
-    await prisma.user.update({
-      where: {
-        id: decodedToken.uid,
-      },
-      data: {
-        points: userScore,
-      },
-    });
-
-    res.status(200).json(user?.points);
+    res.status(200).json({ message: "Score updated" });
   } catch (error) {
     if (error instanceof Error) {
       res.status(400).json(error.message);
