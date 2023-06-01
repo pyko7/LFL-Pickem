a48a2e5d3dcc8325c13c775f5efb6a7ddc38a2dc
julien henry <julienhenry967@gmail.com>
2023-06-01 12:49:10 +0200

    feat: add 505 error page

---
+++ client/pages/505.tsx
@@ -0,0 +1,11 @@
+import Link from "next/link";
+
+const Custom505 = () => {
+  return (
+    <div className="w-full flex flex-col items-center  gap-6 text-neutral-light">
+      <p className="text-3xl">Une erreur interne est survenue.</p>
+    </div>
+  );
+};
+
+export default Custom505;
